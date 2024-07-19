4d630d74742cb52452a987218ece295e651e8e4b
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-07-19 09:26:20 -0300

    usestate1

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,26 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-
-
+  const [reverse, setReverse] = useState(false);
+  const [count,setCount] = useState(0);
   const reverseClass = reverse ? 'reverse' : '';
 
+    const handleClick = () =>{
+      setReverse( !reverse)
+    }
+    const handleIncrement = () =>{
+      setCount(count + 1)
+    }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
+      <div className="App">
+       <header>
+
+          <img src={reactLogo}
+          className={`logo ${reverseClass}`} alt="React logo" />
+          <button type="button" onClick = {handleClick} >
+             Reverse
+          </button>
+          <button type="button" onClick = {handleIncrement}  >
+             Increment : {count}
+          </button>
+          <h1>Contador : {count}</h1>
+       </header>
+
+
 
       </div>
+
+
 
     </>
   )
