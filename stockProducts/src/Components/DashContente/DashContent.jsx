a07ffe870749889574633c39f7fdae842d8fde5e
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-07-25 17:16:37 -0300

    Update DashContent.jsx

--- stockProducts/src/Components/DashContente/DashContent.jsx
+++ stockProducts/src/Components/DashContente/DashContent.jsx
@@ -9,6 +9,7 @@ const totalUnd = base.reduce ((count,base)=>{
 return count+base.quantity;
 }, 0)
 
+const totalProducts = base.length
 console.log(base)
   return (
     <div className={style.contentDash}>
@@ -16,7 +17,7 @@ console.log(base)
       <div className={style.dashboard}>
         <div className={style.cardContentDash}>
           <h3 className={style.pcardContent}>Variedade de Itens</h3>
-          <p className={style.cardContentResult} >10</p></div>
+          <p className={style.cardContentResult} >{totalProducts}</p></div>
         <div className={style.cardContentDash}>
           <h3 className={style.pcardContent}>Total de Itens</h3>
           <p className={style.cardContentResult}>{totalUnd}</p></div>
