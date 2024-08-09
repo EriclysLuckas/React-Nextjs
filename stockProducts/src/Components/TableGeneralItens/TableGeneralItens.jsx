dad8c5c95df745ce330c6c5227d6123b0ce08b3b
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-08-09 15:01:00 -0300

    Ajuste do console table general

--- stockProducts/src/Components/TableGeneralItens/TableGeneralItens.jsx
+++ stockProducts/src/Components/TableGeneralItens/TableGeneralItens.jsx
@@ -1,46 +1,46 @@
 import useBaseContext from "../../hooks/userBaseContext";
-import { FaEye,FaTrashAlt, FaPencilAlt   } from "react-icons/fa";
+import { FaEye, FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 import style from "../TableGeneralItens/TableGeneralItens.module.css"
 
 
 
 export default function TableGeneralItens() {
 
-    const {base} = useBaseContext()
+  const { base } = useBaseContext()
 
-  return(
-    <table className = {style.TableGeneralItens}>
+  return (
+    <table className={style.TableGeneralItens}>
 
-    <thead>
-      <tr className = {style.TableGeneralItensTRhead}>
-        <th scope="col">ID  </th>
-        <th scope="col" className = {style.name}> Nome</th>
-        <th scope="col">  Em estoque</th>
-        <th scope="col"> Categoria</th>
-        <th scope="col"> Ações </th>
+      <thead>
+        <tr className={style.TableGeneralItensTRhead}>
+          <th scope="col">ID  </th>
+          <th scope="col" className={style.name}> Nome</th>
+          <th scope="col">  Em estoque</th>
+          <th scope="col"> Categoria</th>
+          <th scope="col"> Ações </th>
 
-      </tr>
-    </thead>
-    <tbody>
-
-      {base.map((products) => (
-
-        <tr key={products.id} >
-          <td>{products.id}</td>
-          <td >{products.name}</td>
-          <td>{products.quantity}</td>
-          <td> {products.category}</td>
-          <div className = {style.btnAction}>
-          <td><button className={style.btnProducts}><FaEye /></button></td>
-          <td><button className={`${style.btnProducts} ${style.edit}`}><FaPencilAlt /></button></td>
-          <td><button className={`${style.btnProducts} ${style.trash}` }><FaTrashAlt/></button></td>
-          </div>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+
+        {base.map((products) => (
+
+          <tr key={products.id} >
+            <td>{products.id}</td>
+            <td >{products.name}</td>
+            <td>{products.quantity}</td>
+            <td> {products.category}</td>
+            <td className={style.tdBtnAll}>
+              <button className={style.btnProducts}><FaEye /></button>
+              <button className={`${style.btnProducts} ${style.edit}`}><FaPencilAlt /></button>
+              <button className={`${style.btnProducts} ${style.trash}`}><FaTrashAlt /></button>
+            </td>
+          </tr>
+        ))}
 
 
-    </tbody>
+      </tbody>
 
-  </table>
+    </table>
   )
 }
